37aeb8f2058bd249f4856d7d48a321e8f2b2e209
nayyer28 <103793255+nayyer28@users.noreply.github.com>
2026-04-16 12:34:43 +0200

    Add: insert-new-interval - submission-6

---
+++ Data Structures & Algorithms/insert-new-interval/submission-6.py
@@ -0,0 +1,36 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # find the slot
+
+        l, r = 0, len(intervals) - 1
+        m = 0
+        overlap = False
+        while l <= r:
+            m = (l + r) // 2
+            if newInterval[0] >= intervals[m][0] and newInterval[0] <= intervals[m][1]:
+                intervals[m][1] = max( intervals[m][1], newInterval[1])
+                overlap = True
+                break
+            elif newInterval[0] <  intervals[m][0]:
+                r = m - 1
+            else:
+                l = m + 1
+        
+        if not overlap:
+            intervals.insert(l, newInterval)
+        
+        length = len(intervals)
+
+        while m + 1 < length and intervals[m][1] >= intervals[m+1][0]:
+            intervals[m][1] = max(intervals[m][1], intervals[m+1][1])
+            intervals.pop(m+1)
+            length = len(intervals)
+
+        return intervals
+
+
+        
+
+
+       
+        
