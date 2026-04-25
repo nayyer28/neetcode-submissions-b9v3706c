15fe03311306d0473a965c4ec7efdccb1506e84d
nayyer28 <103793255+nayyer28@users.noreply.github.com>
2026-04-25 13:14:55 +0200

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        # two pointer solution
+        # aba
+        # abba
+        if not s:
+            return 0
+        # a a a
+        cnt = 0
+        for center in range(len(s)):
+            
+            # odd
+            l = r = center
+            while l > -1 and r < len(s) and s[l] == s[r]:
+                cnt += 1
+                l -= 1
+                r += 1
+            
+            # even
+            l, r = center, center + 1
+            while l > -1 and r < len(s) and s[l] == s[r]:
+                cnt += 1
+                l -= 1
+                r += 1
+        return cnt
