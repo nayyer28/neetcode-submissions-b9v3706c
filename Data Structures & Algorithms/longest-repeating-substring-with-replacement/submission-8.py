d5c10ad7b2f552d45a551f37536903a236268cf3
nayyer28 <103793255+nayyer28@users.noreply.github.com>
2026-06-09 23:58:01 +0200

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.py
@@ -0,0 +1,26 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = right = 0
+        count = {}
+        maxF = 0
+        mxl = 0
+
+        while right < len(s):
+            char = s[right]
+            count[char] = count.get(char,0) + 1
+
+            maxF = max(maxF, count[char])
+
+            if (right - left + 1) - maxF > k:
+                while (right - left + 1) - maxF > k: 
+                    count[s[left]] -= 1
+                    left += 1
+                # now you never evaluate mxl for shrunk windows
+            else: 
+                mxl = max(mxl, (right - left + 1)) # only update mxl when right moves
+            right += 1
+        return mxl
+        
+                
+            
+            
